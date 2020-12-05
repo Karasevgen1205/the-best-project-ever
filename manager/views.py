7572a5029cb1ae87ada4d52036c2e56ee6f8477f
Johnny-Ka <karasevgen1205@yandex.ru>
2020-12-05 12:26:57 +0300

    add "Likes" in models book with add some references and one template and some other tools

--- manager/views.py
+++ manager/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
-from manager.models import Book
+from manager.models import Book, LikeBookUser
 
 
 def hello(request, name='filipp', digit=None):
@@ -20,15 +20,8 @@ class MyPage (View):
 class AddLike(View):
     def get(self, request, id):
         if request.user.is_authenticated:
-            #book = Book.objects.get(id=id)
-            #book.likes += 1
-            #book.save()
-            book = Book.objects.get(id=id)
-            book.likes.add(request.user)
-            book.save()
-            if request.user in book.likes.all():
-                book.likes.filter(user_id=request.user.id).delete()
-            else:
-                book.likes.add(request.user)
-                book.save()
+            try:
+                LikeBookUser.objects.create(user=request.user, book_id=id)
+            except:
+                LikeBookUser.objects.get(user=request.user, book_id=id).delete()
         return redirect("the-main-page")
