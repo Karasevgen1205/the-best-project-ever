ec1a39e260478ed07f213793a0cc867d9a42c132
Johnny-Ka <karasevgen1205@yandex.ru>
2020-12-17 21:35:30 +0300

    adding a form for uploading books 7

--- manager/views.py
+++ manager/views.py
@@ -1,15 +1,16 @@
+from django.contrib.auth import login, logout
 from django.db.models import Count, Prefetch, Avg
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
 from django.contrib.auth.forms import AuthenticationForm
-from manager.forms import BookForm
+from manager.forms import BookForm, CustomAuthenticationForm
 from manager.models import Book, Comment, LikeCommentUser
 from manager.models import LikeBookUser as RateBookUser
 
 
-def hello(request, name='filipp', digit=None):
+def hello(request, name="filipp", digit=None):
     if digit is not None:
         return HttpResponse(f"digit is {digit}")
     return HttpResponse(f"hello {name}")
@@ -18,12 +19,28 @@ def hello(request, name='filipp', digit=None):
 class MyPage(View):
     def get(self, request):
         context = {}
-        comment_query = Comment.objects.annotate(count_like=Count('users_like')).select_related("author")
+        comment_query = Comment.objects.annotate(count_like=Count("users_like")).select_related("author")
         comments = Prefetch("comments", comment_query)
         context['books'] = Book.objects.prefetch_related("authors", comments)
         context['range'] = range(1, 6)
         context['form'] = BookForm()
         return render(request, "index.html", context)
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html", {'form': CustomAuthenticationForm()})
+
+    def post(self, request):
+        user = CustomAuthenticationForm(data=request.POST)
+        if user.is_valid():
+            login(request, user.get_user())
+        return redirect("the-main-page")
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("the-main-page")
 
 
 class AddLike2Comment(View):
@@ -34,28 +51,28 @@ class AddLike2Comment(View):
 
 
 class AddRate2Book(View):
-    def get(self, request, id, rate, location=None):
+    def get(self, request, slug, rate, location=None):
         if request.user.is_authenticated:
-            RateBookUser.objects.create(user=request.user, book_id=id, rate=rate)
+            book_id = Book.objects.get(slug=slug).id
+            RateBookUser.objects.create(user=request.user, book_id=book_id, rate=rate)
         if location is None:
             return redirect("the-main-page")
-        return redirect("book-detail", id=id)
+        return redirect("book-detail", slug=slug)
 
 
 class BookDetail(View):
-    def get(self, request, id):
-        comment_query = Comment.objects.annotate(count_like=Count('users_like')).select_related("author")
+    def get(self, request, slug):
+        comment_query = Comment.objects.annotate(count_like=Count("users_like")).select_related("author")
         comments = Prefetch("comments", comment_query)
-        book = Book.objects.prefetch_related("authors", comments).get(id=id)
-        return render(request, "book_detail.html", {'book': book, "rate": 2})
+        book = Book.objects.prefetch_related("authors", comments).get(slug=slug)
+        return render(request, "book_detail.html", {"book": book, "rate": 2})
 
 
 class AddBook(View):
-   def post(self, request):
+    def post(self, request):
         if request.user.is_authenticated:
             bf = BookForm(data=request.POST)
-            book = bf.save(commit=False)
+            book = bf.save(commit=True)
             book.authors.add(request.user)
             book.save()
         return redirect("the-main-page")
-
