7d5e9b1074add3bbd39fabc7472a62c4ea65e39e
Johnny-Ka <karasevgen1205@yandex.ru>
2020-12-17 21:38:20 +0300

    adding a form for uploading books 10

--- manager/forms.py
+++ manager/forms.py
@@ -1,5 +1,15 @@
-from django.forms import ModelForm, TextInput, Textarea
+from django.forms import ModelForm, TextInput, Textarea, CharField, PasswordInput, BaseForm
 from manager.models import Book
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = UsernameField(widget=TextInput(attrs={'autofocus': True, "class": "form-control"}))
+    password = CharField(
+        label="Password",
+        strip=False,
+        widget=PasswordInput(attrs={'autocomplete': 'current-password', "class": "form-control"}),
+    )
 
 
 class BookForm(ModelForm):
@@ -7,11 +17,10 @@ class BookForm(ModelForm):
         model = Book
         fields = ["title", "text"]
         widgets = {
-        #     "title": TextInput(attrs={"class": "form-control"}),
-        #     "text": Textarea(attrs={"class": "form-control", "rows"=5, "cols"=50})
-        # }
-        # help_texts = {
-        #     "title",
-        #     "text"
-        # }
-
+            "title": TextInput(attrs={"class": "form-control"}),
+            "text": Textarea(attrs={"class": "form-control", "rows": 5, "cols": 50})
+        }
+        help_texts = {
+            "title": "",
+            "text": ""
+        }
