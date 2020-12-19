b82129dddaa4985a993319885f60d4d3a52db003
Johnny-Ka <karasevgen1205@yandex.ru>
2020-12-19 10:41:13 +0300

    adding a form for uploading books 18

--- manager/management/commands/fix_tmp_book.py
+++ manager/management/commands/fix_tmp_book.py
@@ -4,3 +4,23 @@ from manager.models import Book, TMPBook, LikeBookUser, Comment
 
 class Commnd(BaseCommand):
     def hendle(self, *args, **options):
+        books = Book.objects.all()
+        arr = [
+            TMPBook(
+                title=b.title,
+                text=b.text,
+                date=b.date,
+                rate=b.rate,
+                count_rated_users=b.count_rated_users,
+                count_all_stars=b.count_all_stars,
+                slug=b.slug
+            )
+            for b in books
+        ]
+        TMPBook.objects.bulk_create(arr)
+        print('done')
+        query = Book.objects.all().values("slug", "id")
+        all_lbu=LikeBookUser.objects.all()
+        for book in query:
+            pass
+
