1b8a89fb4714043dcc8d9c71f4508832dfc828bc
Johnny-Ka <karasevgen1205@yandex.ru>
2020-12-23 20:51:26 +0300

    creating 2 tests to verify the addition of books whith tests rate

--- manager/tests.py
+++ manager/tests.py
@@ -8,6 +8,8 @@ from manager.models import Book
 class TestMyAppPlease(TestCase):
     def setUp(self):
         self.user = User.objects.create_user("test_name")
+        self.user1 = User.objects.create_user("test_name1")
+        self.user2 = User.objects.create_user("test_name2")
 
     def test_add_book(self):
         self.client.force_login(self.user)
@@ -39,6 +41,8 @@ class TestMyAppPlease(TestCase):
         self.book1.author.add(self.user)
         self.book1.save()
         self.book2 = Book.objects.create(title2="test_title2")
+        self.book2.authors.add(self.user2)
+        self.book2.save()
         self.assertEqual(Book.objects.count(), 2)
         data = {
             "title": "test_title2",
@@ -56,7 +60,39 @@ class TestMyAppPlease(TestCase):
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, 302)
         self.book2.refresh_from_db()
-        self.assertEqual(self.book2.title, data['title'])
-        self.assertEqual(self.book2.text, data['text'])
+        self.assertNotEqual(self.book2.title, data['title'])
+        self.assertNotEqual(self.book2.text, data['text'])
+
+    def test_rate_book(self):
+        self.client.force_login(self.user)
+        self.book1 = Book.objects.create(title1="test_title1")
+        url = reverse('add-rate', kwargs=dict(slug=self.book1.slug, rate=3))
+        self.client.get(url)
+        self.book1.refresh_from_db()
+        self.assertEqual(self.book1.rate, 3)
+        #new user
+        self.client.force_login(self.user1)
+        url = reverse('add-rate', kwargs=dict(slug=self.book1.slug, rate=4))
+        self.client.get(url)
+        self.book1.refresh_from_db()
+        self.assertEqual(self.book1.rate, 3.5)
+        # new user1
+        self.client.force_login(self.user1)
+        url = reverse('add-rate', kwargs=dict(slug=self.book1.slug, rate=4))
+        self.client.get(url)
+        self.book1.refresh_from_db()
+        self.assertEqual(self.book1.rate, 3.5)
+        # new user2
+        self.client.force_login(self.user2)
+        url = reverse('add-rate', kwargs=dict(slug=self.book1.slug, rate=5))
+        self.client.get(url)
+        self.book1.refresh_from_db()
+        self.assertEqual(self.book1.rate, 4)
+        #first user rerate
+        url = reverse("add-rate", kwargs=dict(slug=self.book1.slug, rate=5))
+        self.client.force_login(self.user)
+        self.client.get(url)
+        self.book1.refresh_from_db()
+        self.assertEqual(self.book1.rate, 4)
 
 
